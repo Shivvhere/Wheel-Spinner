15eb96018773c7387c34881b3475933bdef11130
mohit <mohitbalwani5@gmail.com>
2021-08-25 17:04:31 +0530

    Added images and bg

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let container = document.querySelector(".container");
+let menu_btn = document.querySelector(".menu-btn");
+let menu = document.querySelector(".menu");
 let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
 
@@ -6,3 +8,9 @@ btn.onclick = function () {
 	container.style.transform = "rotate(" + number + "deg)";
 	number += Math.ceil(Math.random() * 1000);
 }
+
+function showMenu(){
+	menu.classList.toggle("hide");
+}
+
+menu_btn.addEventListener('click',showMenu);
